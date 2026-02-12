842853d07cc23219fbc0cfb8ace21251d5fd8a58
ntf-sonoma <ntf-sonoma@ntf-sonomas-Mac-Pro.local>
2026-02-12 19:14:22 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,45 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { useSelector } from "react-redux";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const Main = () => {
+  const user = useSelector((state) => state.auth.user);
+  return user ? <HomeScreen /> : <LoginScreen />;
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from "../redux/authSlice";
+
+
+
+const LoginScreen = () => {
+  const dispatch = useDispatch()
+  return (
+    <View style={{ marginTop: 100 }}>
+      <Text>LoginScreen</Text>
+      <Button title='login' onPress={()=>dispatch(login({name:'pavitra'}))} />
+    </View>
+  )
+}
+
+export default LoginScreen
